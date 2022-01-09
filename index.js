83d057d650fe4fe6d046e309947f833a947f769c
Aaron Elias <aaronelias007@gmail.com>
2022-01-09 18:32:15 -0500

    commit tree ba384cd745f9c869ede21aeae938bab93f382343

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.getElementsByTagName("main")[0]
+main.remove()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+newHeader.textContent = "Aaron is the champion"
